2108e823dd6f1fec512dfe21537e9693f7ea2103
2020 <141443619+20noname@users.noreply.github.com>
2025-06-30 22:55:14 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          600: '#7c3aed',
+          700: '#6d28d9',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
