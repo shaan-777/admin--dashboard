0e754011020e0a2eeab05aeea7e846fb27956fdd
2020 <141443619+20noname@users.noreply.github.com>
2025-06-30 23:34:46 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        purple: {
-          600: '#7c3aed',
-          700: '#6d28d9',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
