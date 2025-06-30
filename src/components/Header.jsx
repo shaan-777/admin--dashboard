ca6bbe927c8ce50b474fec55b54292a3a9f58c0e
2020 <141443619+20noname@users.noreply.github.com>
2025-06-30 21:59:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h2 className="text-3xl font-bold text-purple-800 mb-8">Admin Dashboard</h2>
+    <h2 className="text-2xl sm:text-3xl font-bold text-purple-800 mb-6 sm:mb-8">
+      Admin Dashboard
+    </h2>
   )
 }
 
