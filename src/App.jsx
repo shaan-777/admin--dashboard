745cd68d8fa0fa19857689f0818f273463facc22
2020 <141443619+20noname@users.noreply.github.com>
2025-06-30 22:47:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,16 @@
 
 // export default App;
 // src/App.jsx
-import React from "react";
-import AdminDashboard from "./components/AdminDashboard";
-import "./App.css";
+// import React from "react";
+// import AdminDashboard from "./components/AdminDashboard";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-      <AdminDashboard />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+//       <AdminDashboard />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
